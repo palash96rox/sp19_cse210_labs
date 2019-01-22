8db45f0ce7e60ef9e86006735ded39fc255ce5eb
Palash A <palasha.@Palashs-MacBook-Pro.local>
2019-01-22 16:19:19 +0530

    done with 02/5a

--- 02/02_05.py
+++ 02/02_05.py
@@ -1,5 +1,7 @@
-def reverse_stack(stack,rev):
-  pass
+def reverse_stack(stack,rev=[]):
+  l = len(stack)-1
+  if l+1==0: return rev
+  return reverse_stack(stack[:l],rev+[stack[l]])
 
 def reverse_string(string,rev=''):
   l = len(string)-1
@@ -8,3 +10,5 @@ def reverse_string(string,rev=''):
 
 string = input("Enter String: ")
 print(string,"reversed:",reverse_string(string))
+stack = [int(x) for x in input("Enter elems: ").split()]
+print(stack,"reversed:",reverse_stack(stack))
